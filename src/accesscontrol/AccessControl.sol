4f9a7476159f663511a6b457513eb47572074941
Usmann Khan <usmann@usmannkhan.com>
2023-07-18 00:16:44 +0200

    pin compiler version, important for this task

--- src/accesscontrol/AccessControl.sol
+++ src/accesscontrol/AccessControl.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 contract AccessControl {
     // in the event that an owner is not available,
--- src/accesscontrol/RewardsBox.sol
+++ src/accesscontrol/RewardsBox.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {AccessControl} from "./AccessControl.sol";
 
 contract RewardsBox {
-    bytes32 public accessControlHash = 0x1b39c3e30ac0e9fe9cda3e9315480ff6bff1fdba94ea0302844a726fa1d62c03;
+    bytes32 public accessControlHash = 0x52275cb721a97dd9407ac8636da0bbf09e03406462dc83a0065f9a7e374d013f;
     IERC20 public immutable rewardToken;
 
     constructor(address _rewardToken) {
