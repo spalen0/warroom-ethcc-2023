0c61326a86ae3a8611458f4660cf88a730192e15
Usmann Khan <usmann@usmannkhan.com>
2023-07-18 00:13:18 +0200

    Remove unused util

--- src/accesscontrol/CodeHashGetter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-contract CodeHashGetter {
-    function gh(address c) public view returns (bytes32) {
-        return c.codehash;
-    }
-}
