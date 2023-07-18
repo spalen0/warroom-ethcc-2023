ba420e64da404d76b08899b6bc84bcace293785d
spalen0 <spalen@proton.me>
2023-07-18 10:09:15 +0200

    chore: update metamoprhic

--- src/metamorphic/Multiplier.sol
+++ src/metamorphic/Multiplier.sol
@@ -7,8 +7,8 @@ contract Multiplier {
     address public rewardToken;
     uint256 public multiplier;
     address public owner;
-    
-    // constructor(address _weth, uint256 _multiplier) {
+    mapping(address => bool) public claimed;
+
     function init(address _rewardToken, uint256 _multiplier) public {
         require(owner == address(0), "Multiplier: Already initialized");
         rewardToken = _rewardToken;
@@ -16,10 +16,17 @@ contract Multiplier {
         owner = msg.sender;
     }
 
+    function setClaimed(address user, bool status) external {
+        require(msg.sender == owner, "Multiplier: Only owner");
+        claimed[user] = status;
+    }
+
     function multiply(uint256 amount) external returns (uint256 result) {
         result = amount * multiplier;
         require(IERC20(rewardToken).balanceOf(address(this)) >= result, "Unwrapper: Not enough balance");
         require(IERC20(rewardToken).allowance(msg.sender, address(this)) > result, "Unwrapper: Not approved");
+        require(!claimed[msg.sender], "Multiplier: Already claimed");
+        claimed[msg.sender] = true;
         IERC20(rewardToken).transferFrom(msg.sender, address(this), amount);
         IERC20(rewardToken).transfer(msg.sender, result);
     }
